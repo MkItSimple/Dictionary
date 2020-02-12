c79ad610202f34253308b589892e549ae4ddd229
MkItSimple <choanania1@gmail.com>
2020-02-12 17:05:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.dictionary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+//        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) // Press Back Icon
+        {
+            Log.d(TAG, "onOptionsItemSelected: ");
+        }
+
+        if (id == R.id.action_exit) // Press Back Icon
+        {
+            System.exit(0);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
